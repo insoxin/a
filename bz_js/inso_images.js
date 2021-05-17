87a404ebbe416a427c95babd6f1ec2972b9230d1
Redy <192666378@qq.com>
2021-05-17 08:24:02 +0800

    更新 inso_images.js

--- bz_js/inso_images.js
+++ bz_js/inso_images.js
@@ -32,7 +32,7 @@ function init0() {
 
 
 function init() {
-    $.post("http://aibizhi.isoyu.com/v1/" + hv + "/category/" + a + "/" + hv + "?limit=" + b + "&skip=" + c + "&adult=true&first=0&order=" + d, function (data) {
+    $.post("https://aibizhi.isoyu.com/v1/" + hv + "/category/" + a + "/" + hv + "?limit=" + b + "&skip=" + c + "&adult=true&first=0&order=" + d, function (data) {
         var imgs = document.getElementsByClassName("img");
         var col_width = document.getElementById("width");
         var width = col_width.offsetWidth;
