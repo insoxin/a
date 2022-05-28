a5338ceb78515aefa6239ac20ad1f2138fc19613
a <19371836+insoxin@users.noreply.github.com>
2022-05-29 06:10:02 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,15 +5,15 @@ var cacheStorageKey = 'minimal-pwa-8'
 var cacheList = [
   '/',
   "index.html",
-  "https://cdn.bootcss.com/twitter-bootstrap/3.3.7/css/bootstrap.min.css",
+  "https://lib.baomitu.com/twitter-bootstrap/3.4.1/css/bootstrap.min.css",
   "logo.png",
   "pwa-fonts.png",
-  "https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js",
-  "https://cdn.bootcss.com/twitter-bootstrap/3.3.7/js/bootstrap.min.js",
-  "https://cdn.jsdelivr.net/gh/insoxin/bz@main/bz_js/inso_img.js",
-  "https://cdn.jsdelivr.net/gh/insoxin/bz@main/bz_css/inso_bz.css",
-  "https://cdn.jsdelivr.net/npm/darkmode-js@1.5.4/lib/darkmode-js.min.js",
-  "https://cdn.jsdelivr.net/gh/insoxin/API@master/js/v1.js",
+  "https://lib.baomitu.com/jquery/3.6.0/jquery.min.js",
+  "https://lib.baomitu.com/twitter-bootstrap/3.4.1/js/bootstrap.min.js",
+  "https://fastly.jsdelivr.net/gh/insoxin/bz@main/bz_js/inso_img.js",
+  "https://fastly.jsdelivr.net/gh/insoxin/bz@main/bz_css/inso_bz.css",
+  "https://fastly.jsdelivr.net/npm/darkmode-js@1.5.4/lib/darkmode-js.min.js",
+  "https://fastly.jsdelivr.net/gh/insoxin/API@master/js/v1.js",
   
 ]
 
